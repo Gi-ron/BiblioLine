53e3216244bbb45a5126be157dae142a8a07f889
unknown <andres.quebrada@utp.edu.co>
2023-05-04 17:57:27 -0500

    Modificacion de contraseña front and backend

--- Biblio-Line/store/views/change_password.py
+++ Biblio-Line/store/views/change_password.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import make_password, check_password
+from store.models.customer import Customer
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.views import  View
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
+import string
 
 class ChangePassword(View):
     def get(self, request):
@@ -13,13 +16,59 @@ class ChangePassword(View):
     def post(self, request):
         postData = request.POST
         old_password = postData.get('old_password')
-        print(old_password)
         new_password = postData.get('new_password')
-        print(new_password)
-        confirm_password = postData.get('old_password')
-        print(confirm_password)
+        confirm_password = postData.get('confirm_password')
 
-        return redirect('account')
+        id = request.session.get('customer')
+        customer = Customer.get_customer_by_id(id)
+        checkpassword= check_password(old_password, customer.password)
+        checkpassword2= check_password(new_password, customer.password)
+
+        if checkpassword and not checkpassword2:
+            error_message = self.validateCustomer(new_password, confirm_password)
+
+            if not error_message:
+                print(old_password)
+                print(new_password)
+                print(confirm_password)
+
+                new_password = make_password (new_password)
+                customer.change_password(new_password)
+
+                
+                return render(request, 'change_password.html', {'good': 'The password was changed successfully'})
+            else:
+                data = {
+                    'error': error_message,
+                }
+
+                return render (request, 'change_password.html', data)
+        else:
+            error_message = 'old password invalid or old and new password are same'
+            data={
+                'error': error_message
+            }
+            return render (request, 'change_password.html', data)
+
+
+
+    
+    def validateCustomer(self, new, confirm):
+        error_message = None
+        if len (new) < 7:
+            error_message = 'Password must be 7 char long'
+        elif not any([char.isdigit() for char in new]):
+            error_message = 'Password requires a digit'
+        elif not any([char.isupper() for char in new]):
+            error_message = 'Password requires a capital'
+        elif not any([True if char in string.punctuation else False for char in new]):
+            error_message = 'Password requires a special character' 
+        elif new != confirm:
+            error_message  = 'The passwords do not match'
+        
+        # saving
+
+        return error_message
     
         # # if request.method == 'POST':
         # #     form = PasswordChangeForm(request.user, request.POST)
