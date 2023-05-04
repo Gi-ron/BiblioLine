0d51bf765a8103dcbf89dbb2e4a10d6a1f35d53d
unknown <andres.quebrada@utp.edu.co>
2023-05-03 20:31:51 -0500

    Vistas de account password

--- Biblio-Line/store/views/change_password.py
+++ Biblio-Line/store/views/change_password.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.views import  View
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
 
-@login_required
-def change_password(request):
-    if request.method == 'POST':
-        form = PasswordChangeForm(request.user, request.POST)
-        if form.is_valid():
-            user = form.save()
-            update_session_auth_hash(request, user)
-            messages.success(request, 'Tu contraseña se actualizó correctamente')
-            return redirect('homepage')
+class ChangePassword(View):
+    def get(self, request):
+        accept = True
+        return render(request, "change_password.html")
+    
+    @login_required
+    def change_password(request):
+        if request.method == 'POST':
+            form = PasswordChangeForm(request.user, request.POST)
+            if form.is_valid():
+                user = form.save()
+                update_session_auth_hash(request, user)
+                messages.success(request, 'Tu contraseña se actualizó correctamente')
+                return redirect('homepage')
+            else:
+                messages.error(request, 'Asegurese de corregir los errores antes de enviar la información.')
         else:
-            messages.error(request, 'Asegurese de corregir los errores antes de enviar la información.')
-    else:
-        form = PasswordChangeForm(request.user)
-    return render(request, 'change_password.html', {'form': form})
+            form = PasswordChangeForm(request.user)
+        return render(request, 'change_password.html', {'form': form})
